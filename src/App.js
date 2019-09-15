8209397da9acbd2a52e27f31c4dcd1eabe06c270
HaLL TsuNaMi <halltsunami@gmail.com>
2019-09-15 15:09:54 -0500

    UI progress
    slowly making the UI for the chat app, then backend will be made for it.

--- src/App.js
+++ src/App.js
@@ -1,26 +1,25 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { NONAME } from 'dns';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<div className="App">
+			<h1 style={{textAlign: "center"}}>
+				Lotus Chat
+			</h1>
+			<div className="textBox">
+				<input
+					className="Text"
+					type="text"
+					placeholder=" plz type here UwU"
+					style={{paddingTop: "0.5%", paddingBottom: "0.5%", border: "none"}}
+				/>
+				<button className="enterButton" style={{backgroundColor: "#af19bf", border: "none", color: "white", paddingTop: "0.5%", paddingBottom: "0.5%"}}> Enter UwU </button>
+			</div>
+		</div>
+	);
 }
 
 export default App;
